9496eebd4685f6583a9f517da1f002d78cbb28fe
Meet Idnani <meetidnani@gmail.com>
2018-08-15 14:08:48 +0200

    Add new files for gateway demo

---
+++ src/main/java/com/relay42/simplemicro/SimplemicroApplication.java
@@ -0,0 +1,12 @@
+package com.relay42.simplemicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemicroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/relay42/simplemicro/SimplemicroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.relay42.simplemicro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimplemicroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
